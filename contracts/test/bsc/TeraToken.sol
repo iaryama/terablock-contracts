f71d32dfccd956f99beb415dda21dc5ab9f5adf8
aster2709 <ymehta777@gmail.com>
2021-09-07 11:55:54 +0530

    feat: new erc20 token exposing both mint and burn

--- contracts/test/bsc/TeraToken.sol
+++ contracts/test/bsc/TeraToken.sol
@@ -7,11 +7,32 @@ import "../../utils/AccessProtected.sol";
 
 // Tera Block Token
 contract TeraToken is ERC20("TeraToken", "TRA"), AccessProtected {
+    address public depositAdmin;
+
+    constructor(address _depositAdmin) public {
+        depositAdmin = _depositAdmin;
+    }
+
     function mint(address to, uint256 amount) external onlyAdmin {
         _mint(to, amount);
     }
 
-    function getOwner() external view returns (address) {
-        return owner();
+    function withdraw(uint256 _amount) external {
+        _burn(_msgSender(), _amount);
+    }
+
+    function deposit(address user, bytes calldata depositData) external {
+        require(_msgSender() == depositAdmin, "sender != depositAdmin");
+        uint256 amount = abi.decode(depositData, (uint256));
+        _mint(user, amount);
+    }
+
+    function setDepositAdmin(address _newDepositAdmin) external onlyAdmin {
+        depositAdmin = _newDepositAdmin;
+    }
+
+    /// Withdraw any IERC20 tokens accumulated in this contract
+    function withdrawTokens(IERC20 _token) external onlyOwner {
+        _token.transfer(owner(), _token.balanceOf(address(this)));
     }
 }
