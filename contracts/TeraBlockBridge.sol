c9c73042dfea9e7948d81fd35bd052bd79162409
Aryama <aryu14@gmail.com>
2021-10-13 16:42:47 +0530

    feat: added meta txs contracts

--- contracts/TeraBlockBridge.sol
+++ contracts/TeraBlockBridge.sol
@@ -2,9 +2,11 @@
 
 pragma solidity 0.6.12;
 
-import "./utils/AccessProtected.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./utils/ContextMixin.sol";
+import "./utils/NativeMetaTransaction.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ITeraBlockToken {
     /**
@@ -14,11 +16,52 @@ interface ITeraBlockToken {
 }
 
 // Tera Block Bridge
-contract TeraBlockBridge is AccessProtected, Pausable, ReentrancyGuard {
+contract TeraBlockBridge is Pausable, ReentrancyGuard, ContextMixin, NativeMetaTransaction, Ownable {
     ITeraBlockToken token;
 
     constructor(ITeraBlockToken _token) public {
+        _initializeEIP712("TeraBlockBridge");
         token = _token;
+    }
+
+    mapping(address => bool) private _admins; // user address => admin? mapping
+
+    event AdminAccessSet(address _admin, bool _enabled);
+
+    /**
+     * @notice Set Admin Access
+     *
+     * @param admin - Address of Minter
+     * @param enabled - Enable/Disable Admin Access
+     */
+    function setAdmin(address admin, bool enabled) external onlyOwner {
+        _admins[admin] = enabled;
+        emit AdminAccessSet(admin, enabled);
+    }
+
+    /**
+     * @notice Check Admin Access
+     *
+     * @param admin - Address of Admin
+     * @return whether minter has access
+     */
+    function isAdmin(address admin) public view returns (bool) {
+        return owner() == admin || _admins[admin];
+    }
+
+    /**
+     * Throws if called by any account other than the Admin.
+     */
+    modifier onlyAdmin() {
+        require(_admins[_msgSender()] || _msgSender() == owner(), "Caller does not have Admin Access");
+        _;
+    }
+
+    /**
+     * This is used instead of msg.sender
+     */
+    function _msgSender() internal view override returns (address payable) {
+        return ContextMixin.msgSender();
     }
 
     function deposit(address user, uint256 amount) external onlyAdmin nonReentrant whenNotPaused {
