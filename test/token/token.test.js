7fec446de35a93d1ebf7f81d13e02c08a27eb013
Aryama <aryu14@gmail.com>
2021-10-16 13:39:51 +0530

    feat: removed unused code

--- test/token/token.test.js
+++ test/token/token.test.js
@@ -60,27 +60,6 @@ const depositABI = {
     type: "function",
 }
 
-function dec2hex(str) {
-    // .toString(16) only works up to 2^53
-    var dec = str.toString().split(""),
-        sum = [],
-        hex = [],
-        i,
-        s
-    while (dec.length) {
-        s = 1 * dec.shift()
-        for (i = 0; s || i < sum.length; i++) {
-            s += (sum[i] || 0) * 10
-            sum[i] = s % 16
-            s = (s - sum[i]) / 16
-        }
-    }
-    while (sum.length) {
-        hex.push(sum.pop().toString(16))
-    }
-    return hex.join("")
-}
-
 const getTransactionData = async (user, nonce, abi, domainData, params) => {
     const functionSignature = web3Abi.encodeFunctionCall(abi, params)
 
