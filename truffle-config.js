0de133751e14f878b1484427397167dc320000ff
Aryama <aryu14@gmail.com>
2021-11-05 11:48:18 +0530

    feat: final working meta tx

--- truffle-config.js
+++ truffle-config.js
@@ -64,7 +64,7 @@ module.exports = {
     },
     plugins: ["truffle-plugin-verify"],
     api_keys: {
-        etherscan: "X949QYKBMIF8NN4SVEN4XBANMD6SCDUZ6Q",
-        bscscan: "RWXR89XH3ZZQPSXH37YFMPGUIGUC6GXDPU",
+        etherscan: process.env.ETHER_SCAN_API_KEY,
+        bscscan: process.env.BSC_SCAN_API_KEY,
     },
 }
