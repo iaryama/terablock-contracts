e38d83897e9b17aa3dee9d5e7bb7a0bf903b9676
Aryama <aryu14@gmail.com>
2021-11-12 13:38:00 +0530

    feat: optimised code

--- migrations/4_deploy_multi_coin_bridge.js
+++ migrations/4_deploy_multi_coin_bridge.js
@@ -1,6 +1,6 @@
 var MultiCoin = artifacts.require("MultiCoin")
 var MultiCoinLock = artifacts.require("MultiCoinLock")
 module.exports = async function (deployer, network, accounts) {
-    await deployer.deploy(MultiCoin, web3.utils.toWei("10000000", "ether"))
+    await deployer.deploy(MultiCoin, web3.utils.toWei("100000000", "ether"))
     await deployer.deploy(MultiCoinLock, MultiCoin.address)
 }
